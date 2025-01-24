a83028c2e2518b188fa5aac6b497e7280864b481
Rajat Sandeep <93584596+rajatsandeepsen@users.noreply.github.com>
2025-01-24 07:11:01 +0530

    feat: openapi fetcher

--- server.py
+++ server.py
@@ -7,7 +7,12 @@ from starlette.middleware.cors import CORSMiddleware
 from api.main import api_router
 from utils.source import config
 
-app = FastAPI(title="RADAR")
+app = FastAPI(
+    title="RADAR", 
+    openapi_url="/api/openapi.json", 
+    docs_url="/api/swagger",
+    redoc_url="/api/redoc"
+)
 
 if config["ENV"] == "production":
     app.mount("/", StaticFiles(directory="web/dist/", html=True), name="static")
@@ -34,4 +39,4 @@ app.include_router(api_router, prefix="/api")
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
 
-# uv run fastapi dev server.py
+# uv run --env-file .env fastapi dev server.py
